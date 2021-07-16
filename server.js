e7bc6ec661986bf245193831938a8988949834b2
bekzemed <bereketzemed@gmail.com>
2021-07-16 10:26:06 +0300

    write post build script for heroku, serve static asset and change our application title

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const employee = require('./route/employee');
 const cors = require('cors');
+const path = require('path');
 
 //
 const app = express();
@@ -27,6 +28,16 @@ mongoose
 
 // routes
 app.use('/employees', employee);
+
+// serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
